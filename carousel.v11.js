a21b1dcefca736b1ea13001949e66cb1dc6abca1
lufi <yassinefarroud@protonmail.com>
2025-09-16 15:23:42 +0100

    v7

--- carousel.v11.js
+++ carousel.v11.js
@@ -1,3 +1,5 @@
+const ARROW_LEFT_ICON = `<svg version="1.1" id="icons_1_" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve"><style>.st0{display:none}.st1{display:inline}.st2{fill:#FFFFFF}</style><g id="row2_1_"><g id="_x31__4_"><path class="st2" d="M64 .3C28.7.3 0 28.8 0 64s28.7 63.7 64 63.7 64-28.5 64-63.7S99.3.3 64 .3zm0 121C32.2 121.3 6.4 95.7 6.4 64 6.4 32.3 32.2 6.7 64 6.7s57.6 25.7 57.6 57.3c0 31.7-25.8 57.3-57.6 57.3zm1.3-82.8L41.6 64l23.6 25.5h13.5L54.4 64l24.4-25.5H65.3z" id="left_3_"/></g></g></svg>`;
+const ARROW_RIGHT_ICON = `<svg version="1.1" id="icons_1_" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 128 128" xml:space="preserve"><style>.st0{display:none}.st1{display:inline}.st2{fill:#FFFFFF}</style><g id="row2_1_"><g id="_x31__4_"><path class="st2" d="M64 .3C28.7.3 0 28.8 0 64s28.7 63.7 64 63.7 64-28.5 64-63.7S99.3.3 64 .3zm0 121C32.2 121.3 6.4 95.7 6.4 64 6.4 32.3 32.2 6.7 64 6.7s57.6 25.7 57.6 57.3c0 31.7-25.8 57.3-57.6 57.3zm1.3-82.8L41.6 64l23.6 25.5h13.5L54.4 64l24.4-25.5H65.3z" id="left_3_"/></g></g></svg>`;
 const header = document.querySelector("#carouselExampleCaptions");
 if (header) {
   const carousel = document.createElement("div");
@@ -9,7 +11,7 @@ if (header) {
 
             <div class="item" style="background-image: url(https://i.ibb.co/Jjb5HPcZ/h1-slide-02-1.webp);">
                 <div class="content">
-                    <div class="title">SLIDER 1</div>
+                    <div class="title">Organic Handmade Soap</div>
                     <div class="name">EAGLE</div>
                     <div class="des">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa similique consequuntur, reprehenderit dicta repudiandae.</div>
                 </div>
@@ -49,8 +51,8 @@ if (header) {
 
         <!--next prev button-->
         <div class="arrows">
-            <button class="prev"><</button>
-            <button class="next">></button>
+            <button class="prev">${ARROW_LEFT_ICON}</button>
+            <button class="next">${ARROW_RIGHT_ICON}</button>
         </div>
   `;
 
